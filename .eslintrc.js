32dbceca61c292a9d9cfb4a7eb508f9c5b674f6f
ttran0012 <60831602+ttran0012@users.noreply.github.com>
2021-12-15 17:54:05 -0600

    'master'

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": "plugin:react/recommended",
+    "parserOptions": {
+        "sourceType": "module",
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 13
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+    }
+};
