54466b4faf935a4ac5960a86473110a05f7ffe6a
ttran0012 <60831602+ttran0012@users.noreply.github.com>
2021-12-16 17:08:05 -0600

    continue

--- App.js
+++ App.js
@@ -1,60 +1,54 @@
 
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, Button  } from 'react-native';
+import { 
+  StyleSheet, 
+  View, 
+  Button,  
+  FlatList  
+} from 'react-native';
+
+import GoalItem from './components/GoalItem';
+import GoalInput from './components/GoalInput';
 
 export default function App() {
-  const [enteredGoal, setEnteredGoal] = useState('');
   const [courseGoals, setCourseGoals] = useState([]);
+  const [isAddMode, setIsAddMode] = useState('false');
 
-  const goalInputHandler = (enteredText) => {
-    setEnteredGoal(enteredText);
+  const addGoalHandler = goalTitle => {
+    setCourseGoals(currentGoals => [
+      ...currentGoals, 
+      { id: Math.random().toString(), value: goalTitle }
+    ]);
   };
 
-  const addGoalHandler = () => {
-    setCourseGoals(currentGoals => [...currentGoals, enteredGoal]);
+  const removeGoalHandler = goalId => { 
+    setCourseGoals(currentGoals => {
+      return currentGoals.filter((goal) => goal.id !== goalId);
+    });
   };
 
   return (
     <View style={styles.screen}>
-        <View style={ styles.inputContainer }>
-          <TextInput 
-            placeholder="Course Goal" 
-            style={styles.input} 
-            onChangeText={goalInputHandler} 
-            value={enteredGoal}
+      <Button title="Add New Goal" onPress={() => setIsAddMode(true)}   />
+      <GoalInput visible={isAddMode} onAddGoal={addGoalHandler} />
+        <FlatList 
+          keyExtractor={(item, index) => item.id}
+          data={courseGoals} 
+          renderItem={itemData => 
+            <GoalItem 
+              id={itemData.item.id} 
+              onDelete={removeGoalHandler} 
+              title={itemData.item.value} 
+              />
+            } 
           />
-           <Button title="ADD" onPress={addGoalHandler} />
-        </View>
-        <View>
-         {courseGoals.map(goal => ( 
-           <View key={goal} style={styles.listItem}>
-            <Text>{goal}</Text>
-         </View> ))}
-        </View>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   screen: {
     padding: 50
-  },
-  inputContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center'
-  },
-  input: { 
-    
-    width: '80%', 
-    borderColor: 'black', 
-    borderWidth: 1 
-  },
-  listItem: {
-    padding: 10,
-    margin:10,
-    backgroundColor: '#ccc',
-    borderColor: 'black',
-    borderWidth: 1
   }
+
 });
